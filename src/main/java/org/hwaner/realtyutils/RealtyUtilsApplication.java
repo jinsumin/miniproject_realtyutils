d2ab2b5eb3a1cc1e56a170f88f25fcf07bdce642
hwaner <ljhwan8111@naver.com>
2022-04-14 01:27:57 +0900

    start

---
+++ src/main/java/org/hwaner/realtyutils/RealtyUtilsApplication.java
@@ -0,0 +1,13 @@
+package org.hwaner.realtyutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtyUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hwaner/realtyutils/RealtyUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hwaner.realtyutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtyUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
